e9069d34f39b322f8853c682daceb374744cbe65
Angjoo Kanazawa <angjoo@angjoo-netbook.(none)>
2011-12-03 14:37:12 -0500

    add likelihood

--- trf.m
+++ trf.m
@@ -3,19 +3,13 @@ function [alpha,sig,beta,mu,delta] = trf(data, Learn)
 % Copyright (c) 2004 Daichi Mochihashi, all rights reserved.
 % 
 % Modified by Angjoo Kanazawa/Austin Myers/Abhishek Sharma
-% 
-% data      : data of documents
-% k      : # of classes to assume
-% emmax  : # of maximum VB-EM iteration (default 100)
-% demmax : # of maximum VB-EM iteration for a document (default 20)
-if nargin < 4
-  demmax = 20;
-  if nargin < 3
-    emmax = 100;
-  end
-end
-n = length(d);
+%
+% Look at the config file for the default parameter settings and their size
+
+n = length(data);
+
 l = features(d);
+
 beta = ones(l,k) / l;
 alpha = normalize(fliplr(sort(rand(1,k))));
 gammas = zeros(n,k);
@@ -29,18 +23,23 @@ fprintf(1,'number of latent classes = %d\n', k);
 
 for j = 1:emmax
   fprintf(1,'iteration %d/%d..\t',j,emmax);
-  % vb-estep
+
+  %% vb-estep
   betas = zeros(l,k);
+
   for i = 1:n
     [gamma,q] = vbem(d{i},beta,alpha,demmax);
     gammas(i,:) = gamma;
     betas = accum_beta(betas,q,d{i});
   end
   % vb-mstep
+
   alpha = newton_alpha(gammas);
   beta = mnormalize(betas,1);
+
   % converge?
-  lik = lda_lik(d,beta,gammas);
+  lik = trf_lik(d,beta,gammas);
+
   fprintf(1,'likelihood = %g\t',lik);
   if (j > 1) && converged(lik,plik,1.0e-4)
     if (j < 5)
