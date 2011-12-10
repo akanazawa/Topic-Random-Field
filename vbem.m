ac49b935cb63f8ceb1bd3178cb18d42449c24876
Angjoo <kanazawa@umiacs.umd.edu>
2011-12-10 18:12:12 -0500

    add move converged up to trf

--- vbem.m
+++ vbem.m
@@ -11,14 +11,14 @@ gamma = zeros(1,k);
 xi = zeros(l,k);
 rho = zeros(1,k);
 lambda = 1;
-keyboard
+
 pxi = 0;
 prho = 0;
 for j = 1:Learn.V_Max_Iterations
   % vb-estep
-  gamma = rand(1,k)
-  xi = rand(l,k)
-  rho = rand(1,k)
+  gamma = rand(1,k);
+  xi = rand(l,k);
+  rho = rand(1,k);
   lambda = rand(1);
   % converge?
   if (j > 1) && converged(pxi,xio,1.0e-2) && converged(pro,rho,1.0e-2)
